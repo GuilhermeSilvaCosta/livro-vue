c48402f9337f2388affa52dd2441623348a8a315
Guilherme <tec.guilherme.dsc@gmail.com>
2019-03-25 01:20:54 -0300

    Capitulo 3 concluido

---
+++ my-vue-app/src/MyMenu.vue
@@ -0,0 +1,19 @@
+<template>
+	<div @click="onMenuClick">
+		<h4>{{ title }}</h4>
+		<div>
+			<slot></slot>
+		</div>
+	</div>  
+</template>
+<script>
+export default {    
+	props: ['title'],
+	methods:{
+		onMenuClick : function(e){			
+			this.$emit('menu-click',"emit event from menu");
+		}
+	}
+}
+</script>
+
